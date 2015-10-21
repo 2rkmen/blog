a4440f23db38b8acc0d6e10f8a3a350069ca8fd6
2rkmen <maotsedun@mail.ru>
2015-10-21 22:29:05 +0300

    функционал добавления статьи

--- models/articles.php
+++ models/articles.php
@@ -34,8 +34,31 @@ function articles_get($link, $id_article){
 	return $article;
 }
 
-function articles_new($title, $date, $content){
+function articles_new($link, $title, $date, $content){
+	
+	//готовим
+	$title = trim($title);
+	$content = trim($content);
 
+	//validation
+	if ($title == '')
+		return false;
+
+	//query
+	$t = "INSERT INTO articles (title, date, content) VALUES ('%s', '%s', '%s')";
+
+	$query = sprintf($t, 
+		mysqli_real_escape_string($link, $title), 
+		mysqli_real_escape_string($link, $date),
+		mysqli_real_escape_string($link, $content));
+
+	echo $query;
+	$result = mysqli_query($link, $query);
+
+	if(!$result)
+		die(mysqli_error($link));
+
+	return true;
 }
 
 function articles_edit($id, $title, $date, $content){
